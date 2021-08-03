777540108196cca6a69c798210650810b1582f7b
Judd Mehr <juddmehr@gmail.com>
2021-08-03 11:21:37 -0600

    try fixing banner close

--- js/main.js
+++ js/main.js
@@ -48,10 +48,6 @@ $(document).ready(function(){
 
 });
 
-$(document).querySelector(".banner__close").addEventListener("click", function () {
-  this.closest(".banner").style.display = "none";
-});
-
 
 
 
